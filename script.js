370364f349862c8f69c6176465b67e0aab116621
guilhermerodrigues <guilhermerodrigues@FCA-TI13>
2023-11-20 09:23:12 -0300

    msgErro-Sucesso

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 function BuscarDadosAPI() {
   var cpfInput = document.getElementById('inputCPF').value;
-  var msg = document.getElementById('Mensagem').value;
   fetch('https://localhost:44306/api/Verificador?CPF=' + cpfInput)
-      .then((response) => {
-          return response.json();
-      })
-      .then((myContent) => {
-          if (myContent['valido'] == false) {
-              cpfOK.style.display = "none";
-          }
-          else {
-              cpfOK.style.display = "block";          
-          }
-          CPFFormatado.innerHTML = myContent['cpf'];
-          OrigemCPF.innerHTML = myContent['origem'];
-      });
+    .then((response) => {
+      return response.json();
+    })
+    .then((myContent) => {
+      if (myContent['valido'] == false) {
+        cpfOK.style.display = "none";
+        msgSucesso.style.display = "none";
+        msgErro.style.display = "block";
+        respostaErro.innerHTML =  myContent['mensagem'];
+      }
+      else {
+        cpfOK.style.display = "block";
+        msgSucesso.style.display = "block";
+        msgErro.style.display = "none";
+      }
+      CPFFormatado.innerHTML = myContent['cpf'];
+      OrigemCPF.innerHTML = myContent['origem'];
+
+      var data = new Date(myContent['dataHora']);
+      var dataFormatada = `${padZero(data.getDate())}/${padZero(data.getMonth() + 1)}/${data.getFullYear()} ${padZero(data.getHours())}:${padZero(data.getMinutes())}:${padZero(data.getSeconds())}`;
+      DataHora.innerHTML = dataFormatada;
+    });
+}
+
+function padZero(num) {
+  return num < 10 ? `0${num}` : num;
 }
 
 function Inicio() {
-  cpfOK.style.display = "none"
+  cpfOK.style.display = "none";
+  msgSucesso.style.display = "none";
+  msgErro.style.display = "none";
 }
 
 window.onload = Inicio;
